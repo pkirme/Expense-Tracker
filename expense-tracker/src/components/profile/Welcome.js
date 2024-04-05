5a41c932f70cc3a3c04a2bf6049c6d168f894802
pkirme <kirmepriti@gmail.com>
2024-04-05 17:32:52 +0530

    Verify User Email

--- expense-tracker/src/components/profile/Welcome.js
+++ expense-tracker/src/components/profile/Welcome.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
 
 const Welcome = () => {
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  const onClickHandler = async () => {
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBhWgo-onnehVfjggey6b2N9Rel6F0txZc";
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+          returnSecureToken: true,
+        }),
+        header: {
+          "Content-type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        let errorMsg = "Varification Fail!!";
+        throw new Error(errorMsg);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
   return (
     <Container className="my-5">
       <Row>
@@ -23,6 +54,15 @@ const Welcome = () => {
         </Col>
       </Row>
       <hr />
+      <Row>
+        <Col>
+          <div className="d-grid gap-2">
+            <Button variant="secondary" onClick={onClickHandler}>
+              Verify Email Id.
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
